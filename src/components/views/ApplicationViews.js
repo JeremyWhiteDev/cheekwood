fb6588c5d60a602e23d794f0b2689ea2d259335c
Jeremy White <jcwhite414@gmail.com>
2023-01-25 07:21:19 -0600

    removed unused routes and imports

--- src/components/views/ApplicationViews.js
+++ src/components/views/ApplicationViews.js
@@ -4,9 +4,7 @@ import { AboutUs } from "../aboutUs/AboutUs";
 import { EventDetails } from "../events/EventDetails";
 import { EventForm } from "../events/EventForm";
 import { Map } from "../map/Map";
-import { Event } from "../events/Event";
 import { Profile } from "../profile/Profile";
-import { CommentEdit } from "../profile/EditComments";
 import { UserForm } from "../profile/UserForm";
 import { UserComments } from "../profile/UserComments";
 import { QuestionPage } from "../questions/QuestionPage";
@@ -17,22 +15,15 @@ export const ApplicationViews = () => {
 
   return (
     <Routes>
-      {/* <Route path="/about-us" element={<>about-us </>} /> */}
-      {/* <Route path="/map" element={<>map</>} /> */}
       <Route path="/events" element={<EventList />} />
       <Route path="/event/:eventId" element={<EventDetails />} />
       <Route path="/profile" element={<Profile />} />
       <Route exact path="/" element={<AboutUs />} />
       <Route path="/aboutUs" element={<AboutUs />} />
       <Route path="/map" element={<Map />} />
-      <Route path="/questions" element={<QuestionPage/>} />
-      {/* <Route path="/" element={<Footer/>} /> */}
-
-      
-      <Route path="/profile/:commentId" element={< UserComments />} />
+      <Route path="/questions" element={<QuestionPage />} />
+      <Route path="/profile/:commentId" element={<UserComments />} />
       <Route path="/profile/userForm" element={<UserForm />} />
-    {/*   <Route path="/profile/userComments" element={<UserComments />} /> */}
-
 
       {projectUserObject.userType === "employee" ? (
         <>
