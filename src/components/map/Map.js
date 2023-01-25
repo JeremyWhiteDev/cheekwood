377697046595a911353d1baeb1d6b35b7a5b5b1a
Jeremy White <jcwhite414@gmail.com>
2023-01-25 07:43:12 -0600

    removed commented out code. Reformatted with Prettier

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -1,43 +1,41 @@
 import "./Map.css";
-//import { Footer } from "../footer/Footer";
 
 export const Map = () => {
-    return (
-        <>
-            <section className="about_mainContainer">
-                <div className="about_border_1"></div>
-                <div>
-                    <img
-                        src="https://cheekwood.org/wp-content/uploads/2015/07/4-1200x425.jpg"
-                        className="about_image"
-                    />
-                    <h1 className="About">Cheekwood Map</h1>
-                </div>
-                <div className="about_border_1"></div>
-            </section>
+  return (
+    <>
+      <section className="about_mainContainer">
+        <div className="about_border_1"></div>
+        <div>
+          <img
+            src="https://cheekwood.org/wp-content/uploads/2015/07/4-1200x425.jpg"
+            className="about_image"
+          />
+          <h1 className="About">Cheekwood Map</h1>
+        </div>
+        <div className="about_border_1"></div>
+      </section>
 
-            <section className="map_logo">
-                <div>
-                    <a href="https://cheekwood.org/visit/cheekwood-map/">
-                        <img
-                            src="https://cheekwood.org/wp-content/themes/cheekwood/images/default.jpg"
-                            className="map_image"
-                            width="30%"
-                            height="30%"
-                        />
-                    </a>
-                </div>
+      <section className="map_logo">
+        <div>
+          <a href="https://cheekwood.org/visit/cheekwood-map/">
+            <img
+              src="https://cheekwood.org/wp-content/themes/cheekwood/images/default.jpg"
+              className="map_image"
+              width="30%"
+              height="30%"
+            />
+          </a>
+        </div>
 
-                <h2>
-                    <a
-                        className="map_cta"
-                        href="https://cheekwood.org/wp-content/uploads/2021/09/Cheekwood-Campus-Map.pdf"
-                    >
-                        Cheekwood <mark>Map</mark>
-                    </a>
-                </h2>
-            </section>
-            {/* {Footer()} */}
-        </>
-    );
+        <h2>
+          <a
+            className="map_cta"
+            href="https://cheekwood.org/wp-content/uploads/2021/09/Cheekwood-Campus-Map.pdf"
+          >
+            Cheekwood <mark>Map</mark>
+          </a>
+        </h2>
+      </section>
+    </>
+  );
 };
