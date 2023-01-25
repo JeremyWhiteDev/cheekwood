3fd5163c51aa6b91d72dcbfaa0e25895b02d959c
Jeremy White <jcwhite414@gmail.com>
2023-01-25 07:41:23 -0600

    removed commented out link. Reformatted with Prettier

--- src/components/nav/Navbar.js
+++ src/components/nav/Navbar.js
@@ -1,39 +1,62 @@
-import { useNavigate, Link } from "react-router-dom/dist"
-import { Event } from "../events/Event"
-import "./Navbar.css"
+import { useNavigate, Link } from "react-router-dom/dist";
+import "./Navbar.css";
 
 export const Navbar = () => {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate()
+  return (
+    <>
+      <nav>
+        <div className="navBar">
+          <img className="logo" src={require("./nav-images/logo.png")} alt="" />
+          <ul>
+            <li className="navBar_item">
+              <Link className="navBar_link" to="/aboutUs">
+                About Us
+              </Link>
+            </li>
+            <li className="navBar_item">
+              <Link className="navBar_link" to="/map">
+                Map
+              </Link>
+            </li>
+            <li className="navBar_item">
+              <Link className="navBar_link" to="/events">
+                Events
+              </Link>
+            </li>
+            <li className="navBar_item">
+              <Link className="navBar_link" to="/profile">
+                Profile
+              </Link>
+            </li>
+            <li className="navBar_item">
+              <Link className="navBar_link" to="/questions">
+                FAQ
+              </Link>
+            </li>
 
-    return <>
-        <nav>
-            <div className="navBar">
-                <img className="logo" src={require("./nav-images/logo.png")} alt="" />
-                <ul>
-                    <li className="navBar_item"><Link className="navBar_link" to="/aboutUs">About Us</Link></li>
-                    <li className="navBar_item"><Link className="navBar_link" to="/map">Map</Link></li>
-                    <li className="navBar_item"><Link className="navBar_link" to="/events">Events</Link></li>
-                    {/* <li className="navBar_item"><Link className="navBar_link" to="">My Events</Link></li> */}
-                    <li className="navBar_item"><Link className="navBar_link" to="/profile">Profile</Link></li>
-                    <li className="navBar_item"><Link className="navBar_link" to="/questions">FAQ</Link></li>
-
-                    {
-                        localStorage.getItem("project_user")
-                            ?
-                            <li className="navBar_item" > <Link className="navBar_link" to="" onClick={
-                                () => {
-                                    localStorage.removeItem("project_user")
-                                    navigate("/", { replace: true })
-                                }
-                            }>Logout</Link>
-
-                            </li> : ""}
-                </ul>
-            </div>
-        </nav>
-        <div className="navSpacer">
+            {localStorage.getItem("project_user") ? (
+              <li className="navBar_item">
+                {" "}
+                <Link
+                  className="navBar_link"
+                  to=""
+                  onClick={() => {
+                    localStorage.removeItem("project_user");
+                    navigate("/", { replace: true });
+                  }}
+                >
+                  Logout
+                </Link>
+              </li>
+            ) : (
+              ""
+            )}
+          </ul>
         </div>
+      </nav>
+      <div className="navSpacer"></div>
     </>
-}
-
+  );
+};
